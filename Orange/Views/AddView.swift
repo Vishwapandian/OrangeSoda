eca47efcef9638cd5c81735c328a3ca1ec59b5fb
Vishwapandian <vishwa.pandian.business@gmail.com>
2022-05-17 12:51:52 -0500

    Fixed float time issue

---
+++ Orange/Views/AddView.swift
@@ -0,0 +1,275 @@
+//
+//  AddEvent.swift
+//  schedule
+//
+//  Created by Vishwa Pandian on 4/1/22.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    
+    let coreDM: CoreDataManager
+    
+    @State var nameOfEvent = ""
+    @State var selectedDate = Date()
+    @State var selectedTime = Date()
+    @State var reccuring = false
+    @State var timeMatters = false
+    
+    @State var totalHours = "0 hours"
+    @State var totalMinutes = "5 minutes"
+    
+    @State var totalHoursInt: Int16 = 0
+    @State var totalMinutesInt: Int16 = 5
+    
+    @Binding var showEventPicker: Bool
+    
+    @State var anytimeBeforeBool = false
+    
+    var body: some View {
+        VStack {
+            
+            Form {
+                
+                Section {
+                    TextField("Name of event", text: $nameOfEvent)
+                }
+                
+                Section(header: Text("Due date")) {
+                    DatePicker("When: ", selection: $selectedDate, in: Date()..., displayedComponents: .date)
+                        .datePickerStyle(.graphical)
+                    
+                    Toggle("Can be anytime before", isOn: $anytimeBeforeBool)
+                    
+                }
+                
+                Section(header: Text("start time")) {
+                    if (!timeMatters) {
+                        DatePicker("",selection: $selectedTime, displayedComponents: .hourAndMinute)
+                            .datePickerStyle(.wheel)
+                    }
+                    Toggle("Time doesn't matter", isOn: $timeMatters)
+                }
+                
+                Section(header: Text("event length")) {
+                    Menu {
+                        
+                        Button {
+                            
+                            totalHours = "0 hours"
+                            
+                        } label: {
+                            Text("0 Hours")
+                        }
+                        
+                        Button {
+                            
+                            totalHours = "1 hour"
+                            totalHoursInt = 1
+                            
+                        } label: {
+                            Text("1 Hour")
+                        }
+                        
+                        Button {
+                            
+                            totalHours = "2 hours"
+                            totalHoursInt = 2
+                            
+                        } label: {
+                            Text("2 Hours")
+                        }
+                        
+                        Button {
+                            
+                            totalHours = "3 hours"
+                            totalHoursInt = 3
+                            
+                        } label: {
+                            Text("3 Hours")
+                        }
+                        
+                        Button {
+                            totalHours = "4 hours"
+                            totalHoursInt = 4
+                        } label: {
+                            Text("4 Hours")
+                        }
+                        
+                        Button {
+                            totalHours = "5 hours"
+                            totalHoursInt = 5
+                        } label: {
+                            Text("5 Hours")
+                        }
+                        
+                        Button {
+                            totalHours = "6 hours"
+                            totalHoursInt = 6
+                        } label: {
+                            Text("6 Hours")
+                        }
+                        
+                        Button {
+                            totalHours = "7 hours"
+                            totalHoursInt = 7
+                        } label: {
+                            Text("7 Hours")
+                        }
+                        
+                        Button {
+                            totalHours = "8 hours"
+                            totalHoursInt = 8
+                        } label: {
+                            Text("8 Hours")
+                        }
+                        
+                        Button {
+                            totalHours = "9 hours"
+                            totalHoursInt = 9
+                        } label: {
+                            Text("9 Hours")
+                        }
+                        
+                    } label: {
+                        Text(totalHours)
+                            .foregroundColor(.secondary)
+                    }
+                    
+                    Menu {
+                        
+                        Button {
+                            totalMinutes = ("0 minutes")
+                            
+                        } label: {
+                            Text("0 minutes")
+                        }
+                        
+                        Button {
+                            totalMinutes = ("5 minutes")
+                            totalMinutesInt = 5
+                        } label: {
+                            Text("5 minutes")
+                        }
+                        
+                        Button {
+                            totalMinutes = ("10 minutes")
+                            totalMinutesInt = 10
+                        } label: {
+                            Text("10 minutes")
+                        }
+                        
+                        Button {
+                            totalMinutes = ("15 minutes")
+                            totalMinutesInt = 15
+                        } label: {
+                            Text("15 minutes")
+                        }
+                        
+                        Button {
+                            totalMinutes = ("20 minutes")
+                            totalMinutesInt = 20
+                        } label: {
+                            Text("20 minutes")
+                        }
+                        
+                        Button {
+                            totalMinutes = ("25 minutes")
+                            totalMinutesInt = 25
+                        } label: {
+                            Text("25 minutes")
+                        }
+                        
+                        Button {
+                            totalMinutes = ("30 minutes")
+                            totalMinutesInt = 30
+                        } label: {
+                            Text("30 minutes")
+                        }
+                        
+                        Button {
+                            totalMinutes = ("40 minutes")
+                            totalMinutesInt = 40
+                        } label: {
+                            Text("40 minutes")
+                        }
+                        
+                        Button {
+                            totalMinutes = ("45 minutes")
+                            totalMinutesInt = 45
+                        } label: {
+                            Text("45 minutes")
+                        }
+                        
+                        Button {
+                            totalMinutes = ("50 minutes")
+                            totalMinutesInt = 50
+                        } label: {
+                            Text("50 minutes")
+                        }
+                        
+                        
+                    } label: {
+                        Text(totalMinutes)
+                            .foregroundColor(.secondary)
+                    }
+
+                }
+                
+                Section {
+                    Toggle("Is this a recurring event?", isOn: $reccuring)
+                }
+                
+                Button {
+                    
+                    let dateFormatter = DateFormatter()
+                    
+                    let dateFormatter2 = DateFormatter()
+                     
+                    dateFormatter.dateFormat = "HH"
+                    dateFormatter2.dateFormat = "mm"
+                     
+                    var result: Float
+                    
+                    if (!timeMatters) {
+                        
+                        let str1 = dateFormatter.string(from: selectedTime)
+                        let str2 = dateFormatter2.string(from: selectedTime)
+                        
+                        let finalFloat = ((Float)(str1) ?? 1) * 60 + ((Float)(str2) ?? 1)
+                        
+                        result = finalFloat
+                        
+                    } else {
+                        result = 0
+                    }
+                    
+                    
+                    totalMinutesInt += (totalHoursInt * 60)
+                    if nameOfEvent == "" {
+                        nameOfEvent = "untitled event"
+                    }
+                    coreDM.saveEvent(name: nameOfEvent, date: selectedDate, repeats: reccuring, length: totalMinutesInt, time: "result", anytime: anytimeBeforeBool, timeFloat: result)
+                    showEventPicker = false
+                } label: {
+                    HStack {
+                        Spacer()
+                        Text("Confirm")
+                            .foregroundColor(.green)
+                            .font(.title)
+                            .bold()
+                        Spacer()
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(coreDM: CoreDataManager(), showEventPicker: .constant(true))
+            .preferredColorScheme(.dark)
+    }
+}
---
+++ Orange/Views/DayView.swift
@@ -0,0 +1,258 @@
+//
+//  DayView.swift
+//  Orange
+//
+//  Created by Vishwa Pandian on 5/13/22.
+//
+
+import SwiftUI
+
+struct DayView: View {
+    
+    @State var currentDate = Date()
+    
+    let coreDM: CoreDataManager
+    
+    var task = Task(name: "test", discription: "discription", date: Date(), priority: 1, dueNow: false, estimatedTime: 0, recurring: false)
+    
+    @State var events: [Event] = [Event]()
+    
+    @State private var showEventPicker = false
+    
+    @State var refreshBool = false
+    
+    func populateCalender() {
+        events = coreDM.getAllEvents()
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    showEventPicker.toggle()
+                } label: {
+                    HStack {
+                        Spacer()
+                        Image(systemName: "plus")
+                        .font(Font.system(size: 20, weight: .bold))
+                        .foregroundColor(.white)
+                        Spacer()
+                    }
+                    .padding()
+                    .background(Color(red: 167/255, green: 206/255, blue: 239/255))
+                    .cornerRadius(15)
+                }
+                .sheet(isPresented: $showEventPicker, onDismiss: {
+                    populateCalender()
+                }) {
+                    AddView(coreDM: CoreDataManager(), showEventPicker: $showEventPicker)
+                        }
+                
+                Button {
+                    populateCalender()
+                } label: {
+                    HStack {
+                        Spacer()
+                        Image(systemName: "shuffle")
+                        .font(Font.system(size: 18, weight: .bold))
+                        .foregroundColor(.white)
+                        Spacer()
+                    }
+                    .padding()
+                    .background(Color(red: 88/255, green: 147/255, blue: 198/255))
+                    .cornerRadius(15)
+                }
+            }
+            .padding(7)
+            HStack {
+                ZStack {
+                    
+                    
+                    HStack {
+                        Button {
+                            currentDate = currentDate - 86400
+                        } label: {
+                            ZStack {
+                                //Circle()
+                                  //  .foregroundColor(Color(red: 84/255, green: 130/255, blue: 170/255))
+                                    //.frame(width: 35, height: 35)
+                                Image(systemName: "chevron.left")
+                                    .foregroundColor(Color(red: 44/255, green: 90/255, blue: 130/255))
+                                  //.foregroundColor(.white)
+                                    .font(Font.system(size: 20, weight: .bold))
+                                
+                            }
+                        }
+                        
+                        
+                        Spacer()
+                            .frame(width: 240)
+                            
+                        
+                        Button {
+                            currentDate = currentDate + 86400
+                        } label: {
+                            ZStack {
+                                //Circle()
+                                  //  .frame(width: 35, height: 35)
+                                    //.foregroundColor(Color(red: 44/255, green: 90/255, blue: 130/255))
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(Color(red: 44/255, green: 90/255, blue: 130/255))
+                                    //.foregroundColor(.white)
+                                    .font(Font.system(size: 20, weight: .bold))
+                            }
+                        }
+                        
+                        
+                    }
+                        
+                        
+                        
+                        Text("\(currentDate.formatted(.dateTime.weekday(.wide).month(.wide).day()))")
+                            .font(Font.system(size: 20, weight: .bold))
+                        
+                        
+                        
+                }
+            }
+            .padding()
+            Spacer()
+            
+            ///*
+            List {
+                
+                ForEach(events, id: \.self) { event in
+                    VStack {
+                        Text(event.name ?? "")
+                        Text((event.date ?? Date()), style: .date)
+                        Text("Length: \(event.length) minutes")
+                        //Text("Time: \(event.time ?? Date())")
+                        Text("Time: \(event.timeFloat)")
+                        if (event.repeats) {
+                            Text("Repeats")
+                        }
+                        if (event.anytime) {
+                            Text("Event can be anytime")
+                        }
+                        
+                    }
+
+                }
+                .onDelete(perform: { IndexSet in
+                    IndexSet.forEach { index in
+                        let event = events[index]
+                        coreDM.deleteEvent(event: event)
+                        populateCalender()
+                    }
+                })
+                
+            }
+            //*/
+            
+            ScrollView {
+                ZStack {
+                    
+                    VStack {
+                        ForEach(0..<24) { time in
+                            
+                            HStack {
+                                VStack(alignment: .trailing) {
+                                    Rectangle()
+                                        .frame(width: 75, height: 1)
+                                        .foregroundColor(.gray)
+                                    if (time > 12) {
+                                        Text("\(time - 12) PM")
+                                            .font(Font.system(size: 15, weight: .thin))
+                                            .foregroundColor(.gray)
+                                            .padding(.bottom, 60)
+                                    } else if (time == 0) {
+                                        Text("12 AM")
+                                            .font(Font.system(size: 15, weight: .thin))
+                                            .foregroundColor(.gray)
+                                            .padding(.bottom, 60)
+                                    } else {
+                                        Text("\(time) AM")
+                                            .font(Font.system(size: 15, weight: .thin))
+                                            .foregroundColor(.gray)
+                                            .padding(.bottom, 60)
+                                    }
+                                } // VStack
+                                Spacer()
+                                
+                                if (refreshBool) {
+                                    
+                                }
+                            } // HStack
+                           
+                        } // ForEach
+                    } // VStack
+                    
+                    VStack {
+                        ForEach(events, id: \.self) { event in
+                            
+                            let compareDate = event.date ?? Date()
+                            
+                            if ("\(currentDate.formatted(.dateTime.weekday(.wide).month(.wide).day()))" == "\(compareDate.formatted(.dateTime.weekday(.wide).month(.wide).day()))")
+                            {
+                                
+                                /*
+                                VStack {
+                                    Text(event.name ?? "")
+                                    Text(compareDate, style: .date)
+                                    Text("Length: \(event.length) minutes")
+                                    //Text("Time: \(event.time ?? Date())")
+                                    Text("Time: \(event.time ?? "N/A")")
+                                    if (event.repeats) {
+                                        Text("Repeats")
+                                    }
+                                    if (event.anytime) {
+                                        Text("Event can be anytime")
+                                    }
+                                    
+                                }
+                                */
+                                
+                                HStack {
+                                    Spacer()
+                                    ZStack {
+                                        
+                                        let len = (CGFloat)(event.length) * 1.55
+                                        
+                                        
+                                        Button {
+                                            
+                                        } label: {
+                                            
+                                            Rectangle()
+                                                .frame(width: 300, height: len)
+                                                .padding(.horizontal)
+                                        }
+                                        
+                                    }
+                                    
+                                    
+                                }
+                                
+                                
+                            } // end of if
+                            
+                        }
+                        Spacer()
+                    }
+                    
+                } // ZStack
+            }
+            
+            
+        }
+    }
+}
+
+
+
+struct DayView_Previews: PreviewProvider {
+    static var previews: some View {
+        DayView(coreDM: CoreDataManager())
+            
+    }
+}
