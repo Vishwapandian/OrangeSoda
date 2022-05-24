3c9c76c6d7caaf3f788d83db07c8e0d17a66a3e0
Vishwapandian <vishwa.pandian.business@gmail.com>
2022-05-24 14:44:52 -0500

    update

--- Orange/ContentView.swift
+++ Orange/ContentView.swift
@@ -8,15 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let coreDM: CoreDataManager
+    
     var body: some View {
-        HStack {
+        TabView {
+            
+            AllTasksView(coreDM: coreDM, events: coreDM.getAllEvents())
+                .tabItem {
+                    Label("First", systemImage: "2.square.fill")
+                  }
+            
+            DayView(coreDM: coreDM)
+                .tabItem {
+                    Label("Second", systemImage: "2.square.fill")
+                  }
             
         }
+        .ignoresSafeArea()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(coreDM: CoreDataManager())
     }
 }
---
+++ Orange/Views/AllTasksView.swift
@@ -0,0 +1,90 @@
+//
+//  AllTasksView.swift
+//  Autotask
+//
+//  Created by Vishwa Pandian on 5/21/22.
+//
+
+import SwiftUI
+
+struct AllTasksView: View {
+    
+    let coreDM: CoreDataManager
+    
+    @State var events: [Event]
+    
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                ScrollViewReader { proxy in
+                    
+                    Text("Past")
+                    ForEach(events, id: \.self) { event in
+                        
+                        if ((Date().formatted(.dateTime.day()) > event.date!.formatted(.dateTime.day()))) {
+                            HStack {
+                                VStack {
+                                    Text(event.name!)
+                                }
+                            }
+                        }
+                        
+                    }
+                    
+                    Text("Today")
+                    ForEach(events, id: \.self) { event in
+                        
+                        if ((Date().formatted(.dateTime.day()) == event.date!.formatted(.dateTime.day()))) {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .frame(width: .infinity, height: 50)
+                                    .padding(.horizontal)
+                                    .foregroundColor(Color("color1"))
+                                
+                                Text("\(event.date!)")
+                            }
+                        }
+                        
+                    }
+                    
+                    Text("Future")
+                    ForEach(events, id: \.self) { event in
+                        
+                        if ((Date().formatted(.dateTime.day()) < event.date!.formatted(.dateTime.day()))) {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .frame(width: .infinity, height: 50)
+                                    .padding(.horizontal)
+                                    .foregroundColor(Color("color1"))
+                                
+                                Text("\(event.date!)")
+                            }
+                        }
+                        
+                    }
+                     
+
+                    Spacer()
+                    
+                }
+            }
+            .navigationTitle("All Tasks")
+        }
+    }
+}
+
+/*
+ RoundedRectangle(cornerRadius: 10)
+     .frame(width: .infinity, height: 50)
+     .padding(.horizontal)
+     .foregroundColor(Color("color1"))
+
+ */
+
+struct AllTasksView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(coreDM: CoreDataManager())
+    }
+}
+
